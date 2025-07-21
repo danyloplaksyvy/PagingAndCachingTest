5cd1ca3b88438897f98ac61d2cac48f34e57a823
Danya Plaksyvy <danyaplaksyvy@gmail.com>
2025-07-21 13:19:58 +0300

    UI implemented

--- app/src/main/java/pro/danbya/pagingandcachingtest/main/presentation/view/MainScreen.kt
+++ app/src/main/java/pro/danbya/pagingandcachingtest/main/presentation/view/MainScreen.kt
@@ -1,23 +1,25 @@
 package pro.danbya.pagingandcachingtest.main.presentation.view
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun MainScreen(onDetailsClick: (Int) -> Unit) {
-    Column(
+    LazyColumn(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Button(onClick = { onDetailsClick(1) }) {
-            Text("Go to Details")
+        items(10) { item ->
+            TextButton(onClick = { onDetailsClick(item) }) {
+                Text("Item #$item")
+            }
         }
     }
 }
--- app/src/main/java/pro/danbya/pagingandcachingtest/navigation/AppNavigation.kt
+++ app/src/main/java/pro/danbya/pagingandcachingtest/navigation/AppNavigation.kt
@@ -2,9 +2,11 @@ package pro.danbya.pagingandcachingtest.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import pro.danbya.pagingandcachingtest.details.presentation.view.DetailsScreen
 import pro.danbya.pagingandcachingtest.main.presentation.view.MainScreen
 
@@ -13,11 +15,18 @@ fun AppNavigation(navController: NavHostController = rememberNavController()) {
     NavHost(startDestination = Screens.MainScreen.name, navController = navController) {
         composable(route = Screens.MainScreen.name) {
             MainScreen { id ->
-                navController.navigate("${Screens.DetailsScreen.name}?$id")
+                navController.navigate("${Screens.DetailsScreen.name}?id=$id")
             }
         }
-        composable(route = "${Screens.DetailsScreen.name}?{id}") {
-            DetailsScreen {
+        composable(
+            route = "${Screens.DetailsScreen.name}?id={id}",
+            arguments = listOf(navArgument("id") {
+                type = NavType.IntType
+                nullable = false
+            })
+        ) {
+            val id = it.arguments?.getInt("id") ?: 0
+            DetailsScreen(id) {
                 navController.popBackStack()
             }
         }
